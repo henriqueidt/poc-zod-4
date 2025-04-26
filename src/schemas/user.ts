ad4aef443a53607dd16b8e9d007b62a1045b33d7
Henrique Eidt <henriqueidt@gmail.com>
2025-04-26 18:35:39 -0300

    continue zod

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,9 +1,9 @@
-import { z } from "zod";
+import { stringbool, z } from "zod";
 
 export const userSchema = z.interface({
-  id: z.string(),
-  name: z.string(),
-  email: z.string().email(),
+  id: z.uuid({ version: "v4" }),
+  name: z.string().min(1).max(50),
+  email: z.email(),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
@@ -37,3 +37,70 @@ coercionBooleanSchema.parse("false"); // true
 coercionBooleanSchema.parse(0); // false
 coercionBooleanSchema.parse(undefined); // false
 coercionBooleanSchema.parse(""); // false
+
+// Literals represents a specific value
+const john = z.literal("John");
+const ten = z.literal(10);
+
+// Strings
+const minMaxStringSchema = z.string().min(1).max(10);
+// String transformations
+const stringTransformationsSchema = z.string().trim().toLowerCase();
+
+// Email
+const emailSchema = z.email();
+// Email custom pattern
+const emailCustomSchema = z.email({
+  pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+});
+// Email with zod patterns
+const emailZodPatternSchema = z.email({ pattern: z.regexes.html5Email });
+
+// UUID
+const uuidSchema = z.uuid({ version: "v4" });
+const uuidSchemaSimple = z.uuidv4();
+
+// ISO datetime (allows "2020-01-01T00:00:00Z")
+const isoDateSchema = z.iso.datetime();
+// with timezone offset (allows "2020-01-01T00:00:00+02:00")
+const isoDateWithTimezoneSchema = z.iso.datetime({ offset: true });
+// timezoneless (allows "2020-01-01T00:00:00")
+const isoDateWithoutTimezoneSchema = z.iso.datetime({ local: true });
+// ISO date (allows "2020-01-01")
+const isoDateSchemaSimple = z.iso.date();
+
+// ISO time (allows "00:00:00")
+const isoTimeSchema = z.iso.time();
+
+// NUMBERS
+const numberSchema = z.number();
+// with min and max
+const numberMinMaxSchema = z.number().min(1).max(10);
+
+// ENUMS
+const animalEnum = z.enum(["dog", "cat", "fish"]);
+// can use ts enums
+enum AnimalEnum {
+  Dog = "dog",
+  Cat = "cat",
+  Fish = "fish",
+}
+const animalEnumTs = z.enum(AnimalEnum);
+// create new enum excluding values
+const waterAnimalEnum = animalEnum.exclude(["dog", "cat"]);
+// create new enum including values
+const landAnimalEnum = animalEnum.extract(["dog", "cat"]);
+
+// Parsing ENV variables (good to parse boolean values for example)
+const strbool = z.stringbool();
+strbool.parse("true"); // true
+strbool.parse("false"); // false
+strbool.parse("1"); // true
+strbool.parse("0"); // false
+strbool.parse("y"); // true
+strbool.parse("n"); // false
+// can customize the truthy and falsy values
+const strboolCustom = z.stringbool({
+  truthy: ["yes", "true", "1"],
+  falsy: ["no", "false", "0"],
+});
